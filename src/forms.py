675012d60c761811a880271f299391bf6cf96ac7
Roy6lty <olowoleru06@gmail.com>
2023-05-01 15:16:37 +0100

    forms2

--- src/forms.py
+++ src/forms.py
@@ -6,7 +6,7 @@ class RegisteredForm(FlaskForm):
     Username = StringField(label= 'Username', validators=[Length(min=2, max=30),DataRequired])
     Email_address = StringField(label= 'Email Address',validators=[Email, DataRequired])
     Password = PasswordField(label= 'Password', validators=[Length(min=6), DataRequired])
-    Confirm_Password = PasswordField(label= 'Confirm Password'validators=[Equalto(Password)])
+    Confirm_Password = PasswordField(label= 'Confirm Password',validators=[Equalto(Password)])
     Create_Account = SubmitField(label = 'Create Account')
     
 
